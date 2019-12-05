27f516f572254ab59d902829da5bb1080d8075e4
kaushik shamantha <kaushik.s@fidenz.com>
2019-12-05 16:46:06 +1100

    Update api_client.rb
    remove `abort` in `generate_access_token`

--- lib/docusign_esign/client/api_client.rb
+++ lib/docusign_esign/client/api_client.rb
@@ -570,8 +570,7 @@ module DocuSign_eSign
           :oauth => true
       }
       data, status_code, headers = self.call_api("POST", '/oauth/token', params)
-      abort(data.inspect)
-
+      data
     end
 
     def set_access_token(token_obj)
